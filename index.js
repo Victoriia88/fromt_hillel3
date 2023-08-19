94aace5e2f90315df52f23a7ed4434d98315d3da
Vulynets88 <vulynets528@gmail.com>
2023-08-19 13:09:01 +0300

    homework22

--- index.js
+++ index.js
@@ -1,45 +1,81 @@
-class Person {
-  constructor(name, gender) {
-    this.name = name;
-    this.gender = gender;
-  }
-}
-
-class Apartment {
-  constructor() {
-    this.residents = [];
+class Student {
+  constructor(firstName, lastName, birthYear) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.grades = [];
+    this.attendance = new Array(25).fill(null);
   }
 
-  addResident(person) {
-    this.residents.push(person);
-  }
-}
-
-class Building {
-  constructor(maxApartments) {
-    this.apartments = [];
-    this.maxApartments = maxApartments;
+  present() {
+    const emptyIndex = this.attendance.indexOf(null);
+    if (emptyIndex !== -1) {
+      this.attendance[emptyIndex] = true;
+    }
   }
 
-  addApartment(apartment) {
-    if (this.apartments.length < this.maxApartments) {
-      this.apartments.push(apartment);
+  absent() {
+    const emptyIndex = this.attendance.indexOf(null);
+    if (emptyIndex !== -1) {
+      this.attendance[emptyIndex] = false;
+    }
+  }
+
+  getAge() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear;
+  }
+
+  getAverageGrade() {
+    if (this.grades.length === 0) {
+      return 0;
+    }
+    const sum = this.grades.reduce((total, grade) => total + grade, 0);
+    return sum / this.grades.length;
+  }
+
+  getAttendancePercentage() {
+    const presentCount = this.attendance.filter((item) => item === true).length;
+    return presentCount / this.attendance.length;
+  }
+
+  summary() {
+    const averageGrade = this.getAverageGrade();
+    const attendancePercentage = this.getAttendancePercentage();
+
+    if (averageGrade > 90 && attendancePercentage > 0.9) {
+      return "Молодець!";
+    } else if (averageGrade > 90 || attendancePercentage > 0.9) {
+      return "Добре, але можна краще";
     } else {
-      console.log("Cannot add more apartments. Maximum capacity reached.");
+      return "Редиска!";
     }
   }
 }
 
-// Демонстрація
-const person1 = new Person("Igor", "male");
-const person2 = new Person("Alla", "female");
+// Приклад використання
+const student1 = new Student("John", "Sigma", 2005);
+const student2 = new Student("Ann", "Vovk", 1994);
 
-const apartment1 = new Apartment();
-apartment1.addResident(person1);
+student1.grades = [95, 55, 92, 88];
+student2.grades = [90, 92, 98, 74];
 
-const apartment2 = new Apartment();
-apartment2.addResident(person2);
+student1.present();
+student1.present();
+student1.absent();
 
-const building = new Building(2);
-building.addApartment(apartment1);
-building.addApartment(apartment2);
+student2.present();
+student2.absent();
+student2.present();
+
+console.log("Student 1 age:", student1.getAge());
+console.log("Student 2 age:", student2.getAge());
+
+console.log("Student 1 average grade:", student1.getAverageGrade());
+console.log("Student 2 average grade:", student2.getAverageGrade());
+
+console.log("Student 1 attendance:", student1.getAttendancePercentage());
+console.log("Student 2 attendance:", student2.getAttendancePercentage());
+
+console.log("Student 1 summary:", student1.summary());
+console.log("Student 2 summary:", student2.summary());
